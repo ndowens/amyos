f5f03ff6a9991841f971f9ebec7a3e56f192676c
Nathan <ndowens@mf.me>
2025-06-17 23:35:47 -0500

    Update install-apps.sh

--- build_files/install-apps.sh
+++ build_files/install-apps.sh
@@ -78,7 +78,7 @@ declare -A RPM_PACKAGES=(
 
   ["brave-browser"]="brave-browser"
   ["cloudflare-warp"]="cloudflare-warp"
-  ["vscode"]="code"
+  ["sneezy"]="floorp"
 )
 
 log "Starting Amy OS build process"
